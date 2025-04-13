205b4d397c4e9f894e2ae2461a65f812b89af70c
maxg100707 <maxguzman914@gmail.com>
2025-04-12 22:57:40 -0300

    Update Average.jsx

--- src/components/Average.jsx
+++ src/components/Average.jsx
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from 'react';
 
 const backendUrl = "https://pluggy.onrender.com"; // Substitua conforme necessário
 
-const Average = ({ trigger }) => {
+const Average = ({ trigger, country }) => {
   const [average, setAverage] = useState(null);
   const [error, setError] = useState(null);
 
   const fetchAverage = async () => {
     try {
-      const response = await fetch(`${backendUrl}/average`);
+      const response = await fetch(`${backendUrl}/average?country=${country}`, {
+        headers: {
+          'country': country
+        }
+      });
       if (!response.ok) {
         throw new Error(`Erro: ${response.statusText}`);
       }
@@ -24,17 +28,20 @@ const Average = ({ trigger }) => {
   useEffect(() => {
     fetchAverage();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [trigger]);
+  }, [trigger, country]);
+
+  // Determina o símbolo da moeda com base no país
+  const currencySymbol = country === 'argentina' ? 'ARS' : 'BRL';
 
   return (
     <div className="card">
-      <h2>Média (Average)</h2>
+      <h2>Média de Cotação (Average)</h2>
       {error ? (
         <p>Erro ao carregar média: {error}</p>
       ) : average ? (
         <div>
-          <p><strong>Média de Compra:</strong> {average.average_buy_price}</p>
-          <p><strong>Média de Venda:</strong> {average.average_sell_price}</p>
+          <p><strong>Média de Compra:</strong> {average.average_buy_price} {currencySymbol}</p>
+          <p><strong>Média de Venda:</strong> {average.average_sell_price} {currencySymbol}</p>
         </div>
       ) : (
         <p>Carregando...</p>
